a0b9e9816942904676e271579451c6ca0b61a6db
VANSHIKA-SONI-20 <sonivanshika630@gmail.com>
2025-04-08 09:14:59 +0530

    add register controller and bug fixes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -34,7 +34,7 @@ const userSchema = new Schema(
         },
         watchHistory:[
             {
-                type:SchemaTypes.Types.ObjectId,
+                type:Schema.Types.ObjectId,
                 ref:"Video"
             }
         ],
